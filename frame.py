5b680335aaf29fd4c6df9615c1e42d2e55ad3b03
David Rice <dwrice0@gmail.com>
2022-01-21 21:47:09 -0500

    Updated cli args and changed masking technique

--- frame.py
+++ frame.py
@@ -3,29 +3,20 @@ from scapy.all import *
 
 def build_pn(packet):
     pn = packet.PN5.to_bytes(1, "big") + packet.PN4.to_bytes(1, "big") + packet.PN3.to_bytes(1, "big") + packet.PN2.to_bytes(1, "big") + packet.PN1.to_bytes(1, "big") + packet.PN0.to_bytes(1, "big")
+    print(f'PN={pn.hex()}')
     return pn
 
 def build_AAD(packet):
     p = bytes(packet.payload)
     offset = 0
-    FC = p[offset:offset+2]
+    FCarray = bytearray(p[offset:offset+2])
     offset += 4
-    #print(f'FC-before = {FC.hex()}')
-    #fc_and_mask_str = '1111000111100010'
-    fc_and_mask_str = '0000111101000111'
-    #fc_and_mask_str = '1000111101000111'
-    fc_and_mask_int = int(fc_and_mask_str, 2)
-    fc_and_mask_hex = fc_and_mask_int.to_bytes(2, "big")
-    print(f'FC-before = {FC.hex()}')
-    print(f'fc_and_mask = {fc_and_mask_hex.hex()}')
-    FC = bytes([a & b for a,b in zip(FC, fc_and_mask_hex)])
-    #print(f'FCand = {FC.hex()}')
-    #fc_or_mask_str = '0000000000000010'
-    fc_or_mask_str = '0000000001000000'
-    fc_or_mask_int = int(fc_or_mask_str, 2)
-    fc_or_mask_hex = fc_or_mask_int.to_bytes(2,"big")
-    print(f'fc_or_mask = {fc_or_mask_hex.hex()}')
-    FC = bytes([a | b for a,b in zip(FC, fc_or_mask_hex)])
+    FCarray[0] = FCarray[0] & 0x0f
+    if packet.haslayer(Dot11QoS):
+        FCarray[1] = (FCarray[1] & 0x47) | 0x40
+    else:
+        FCarray[1] = (FCarray[1] & 0xc7) | 0x40
+    FC = bytes(FCarray)
     print(f'FC = {FC.hex()}')
     A1 = p[offset:offset+6]
     offset += 6
@@ -36,14 +27,11 @@ def build_AAD(packet):
     A3 = p[offset:offset+6]
     offset += 6
     print(f'A3 = {A3.hex()}')
-    SC = p[offset:offset+2]
+    SCarray = bytearray(p[offset:offset+2])
     offset += 2
-    print(f'SC-before = {SC.hex()}')
-    sc_and_mask_str = '0000111100000000'
-    sc_and_mask_int = int(sc_and_mask_str, 2)
-    sc_and_mask_hex = sc_and_mask_int.to_bytes(2, "big")
-    print(f'sc_and_mask = {sc_and_mask_hex.hex()}')
-    SC = bytes([a & b for a,b in zip(SC, sc_and_mask_hex)])
+    SCarray[0] = SCarray[0] & 0x0f
+    SCarray[1] = SCarray[1] & 0x00
+    SC = bytes(SCarray)
     print(f'SC = {SC.hex()}')
     A4 = b''
     if packet.addr4 != None:
@@ -52,12 +40,11 @@ def build_AAD(packet):
         print(f'A4 = {A4.hex()}')
     QC = b''
     if packet.haslayer(Dot11QoS):
-        QC = p[offset:offset+2]
-        print(f'QC-before = {QC.hex()}')
-        qc_and_mask_str = '0000111100000000'
-        qc_and_mask_int = int(qc_and_mask_str, 2)
-        qc_and_mask_hex = qc_and_mask_int.to_bytes(2, "big")
-        QC = bytes([a & b for a,b in zip(QC, qc_and_mask_hex)])
+        QCarray = bytearray(p[offset:offset+2])
+        QCarray[0] = QCarray[0] & 0x0f
+        QCarray[1] = QCarray[1] & 0x00
+        print(f'{QCarray=}')
+        QC = bytes(QCarray)
         print(f'QC = {QC.hex()}')
     AAD = FC + A1 + A2 + A3 + SC + A4 + QC
     return AAD
@@ -68,29 +55,16 @@ def build_nonce(packet):
     if packet.addr4 != None:
         offset = offset + 6
 
-    priority_byte = b'\x00'
-    priority_mask_str = '00000000'
     if packet.haslayer(Dot11QoS):
-        priority_byte = p[offset:offset+1]
-        priority_mask_str = '00001111'
+        priority_byte_array = bytearray(p[offset:offset+1])
+        priority_byte_array[0] = priority_byte_array[0] & 0x0f
+    else:
+        priority_byte_array = bytearray(b'\x00')
     
-    print(f'priority_byte before = {priority_byte.hex()}')
-    priority_mask_int = int(priority_mask_str, 2)
-    priority_mask_hex = priority_mask_int.to_bytes(1, "big")
-    print(f'priority_mask = {priority_mask_hex.hex()}')
-    priority_byte = bytes([a & b for a,b in zip(priority_byte, priority_mask_hex)])
-    print(f'priority_byte = {priority_byte.hex()}')
-    #priority_int = int.from_bytes(priority_byte,byteorder='big')
-    #print(f'priority_int = {priority_int}')
-    #priority_int = priority_int << 4
-    #print(f'priority_int shifted = {priority_int}')
-    #priority_byte = priority_int.to_bytes(1, byteorder='big')
-    #print(f'priority_byte shifted = {priority_byte.hex()}')
     A2 = p[10:16]
     PN = build_pn(packet)
-    nonce = priority_byte + A2 + PN
+    nonce = bytes(priority_byte_array) + A2 + PN
     print(f'nonce = {nonce.hex()}')
-    #print(f'noncelen = {len(nonce)}')
 
     return nonce
 
@@ -99,14 +73,10 @@ def build_clear_Frame(packet, data):
 
 def decrypt_frame(packet, key):
     print(f'key = {key.hex()}')
-    print("Got Data Frame")
     print(f'add1 = {packet.addr1}')
     print(f'add2 = {packet.addr2}')
     print(f'add3 = {packet.addr3}')
     print(f'add4 = {packet.addr4}')
-
-    PN = build_pn(packet)
-    print(f'PN = {PN.hex()}')
 
     AAD = build_AAD(packet)
     print(f'AAD = {AAD.hex()}')
--- start.py
+++ start.py
@@ -13,16 +13,24 @@ def main():
     parser = argparse.ArgumentParser(description='PCAP reader')
     parser.add_argument('--pcap', metavar='<pcap file name>',
             help='pcap file to parse', required=True)
+    parser.add_argument('--key', metavar='<temporal key>',
+            help='decryption key (tk)', required=False)
     args = parser.parse_args()
 
     file_name = args.pcap
+    textkey = args.key
 
     if not os.path.isfile(file_name):
         print('"{}" does not exist',format(file_name), file=sys.stderr)
         sys.exit(-1)
 
-    key = binascii.a2b_hex("c97c1f67ce371185514a8a19f2bdd52f")  #sample.pcapng key
-    #key = binascii.a2b_hex("43e3229c41fec8fb81222388c0b5d3d3")  #single.pcapng key
+    if textkey == None:
+        #key = binascii.a2b_hex("c97c1f67ce371185514a8a19f2bdd52f")  #sample.pcapng key
+        key = binascii.a2b_hex("43e3229c41fec8fb81222388c0b5d3d3")  #single.pcapng key
+    else:
+        key = binascii.a2b_hex(textkey)
+
+
 
     for (pkt_data, pkt_metadata) in RawPcapNgReader(file_name):
         radiotap_pkt = RadioTap(pkt_data)
